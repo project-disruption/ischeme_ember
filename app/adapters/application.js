05b2d17a895921a78f944ad9ad199df5fd30222f
Tony Luisi <t.luisi@gmail.com>
2016-12-03 15:06:18 +1300

    got authentication working

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,18 @@
+
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import config from '../config/environment';
+
 import Ember from 'ember';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 const { String: { pluralize, underscore } } = Ember;
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
 
   pathForType(type) {
     return pluralize(underscore(type));
-  }
+  },
+  host: `${config.host}`,
+  authorizer: 'authorizer:custom'
 
 });
--- config/environment.js
+++ config/environment.js
@@ -25,13 +25,18 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-simple-auth'] = {
+    authorizer: 'authorizer:custom',
+    routeAfterAuthentication: '/'
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    // ENV.host = 'http://localhost:3000';
+    ENV.host = 'http://localhost:4200';
   }
 
   if (environment === 'test') {
